8f76e52c7a6566011b8249ce59b2a6c1d07fae0a
Medya Gh <medyagh@users.noreply.github.com>
2018-06-11 21:45:09 -0700

    Added support for MacOS, Improved Run

--- bash_timer.sh
+++ bash_timer.sh
@@ -1,26 +1,44 @@
- timer () {
-     coremsg="Your "$@"  is up."
-     notifymsg="
-        -------------------
-        -------------------
-        -------------------
-        DING DING DING
-        DING DING DING
-        DING DING DING
-        -------------------
-        $coremsg
-        -------------------
-        DING DING DING
-        DING DING DING
-        DING DING DING
-        -------------------
-        -------------------
-        -------------------
-        "
-     sleep "$@";
-     #TODO check if these are installed before running them
-     notify-send "Medya Timer" "$notifymsg" -u normal;
-     zenity --warning --name "Medya Bash Timer" --text="$coremsg"  > /dev/null 2>&1;
- 
-     #TODO test on other distros other than debian
-  }
+
+timer () {
+    coremsg="Your "$@"  is up."
+    notifymsg="
+       -------------------
+       -------------------
+       -------------------
+       DING DING DING
+       DING DING DING
+       DING DING DING
+       -------------------
+       $coremsg
+       -------------------
+       DING DING DING
+       DING DING DING
+       DING DING DING
+       -------------------
+       -------------------
+       -------------------
+       "
+    sleep "$@";
+
+
+    # Linux
+    path_to_notify_send=$(which notify-send)
+    if [ -x "$path_to_notify_send" ] ; then
+      notify-send "Medya Timer" "$notifymsg" -u normal;
+    fi
+    path_to_zenity=$(which zenity)
+    if [ -x "$path_to_zenity" ] ; then
+      zenity --warning --name "Medya Bash Timer" --text="$coremsg"  > /dev/null 2>&1;
+    fi
+
+    #
+    # For MacOs
+    # TODO use the coremsg for MacOs
+    #
+  
+    path_to_osascript=$(which osascript)
+    if [ -x "$path_to_osascript" ] ; then
+      osascript -e 'display notification "Ding Ding Ding Your time is up !" with title "Medya Timer"'
+      osascript -e 'tell app "System Events" to display dialog "Your time is up !"'
+    fi
+ }
