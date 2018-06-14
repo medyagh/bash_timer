4aa037c6db2ff3735fd08da302c482f4e1765d7f
Medya Gh <medyagh@users.noreply.github.com>
2018-06-14 13:54:59 -0700

    adding xmessage as default for linux
    solves the problem of mouse being in a different monitor. and it is uglier

--- bash_timer.sh
+++ bash_timer.sh
@@ -1,7 +1,6 @@
-
-timer () {
-    coremsg="Your "$@"  is up."
-    notifymsg="
+timer() {
+	coremsg="Your "$@"  is up."
+	notifymsg="
        -------------------
        -------------------
        -------------------
@@ -18,27 +17,32 @@ timer () {
        -------------------
        -------------------
        "
-    sleep "$@";
+	sleep "$@"
 
+	# Linux
+	path_to_notify_send=$(which notify-send)
+	if [ -x "$path_to_notify_send" ]; then
+		notify-send "Medya Timer" "$notifymsg" -u normal
+	fi
+	path_to_xmessage=$(which xmessage)
+	if [ -x "$path_to_xmessage" ]; then
+		xmessage "$notifymsg" -nearmouse
+	else
+		path_to_zenity=$(which zenity)
+		if [ -x "$path_to_zenity" ]; then
+			zenity --warning --name "Medya Bash Timer" --text="$coremsg" >/dev/null 2>&1
+		fi
 
-    # Linux
-    path_to_notify_send=$(which notify-send)
-    if [ -x "$path_to_notify_send" ] ; then
-      notify-send "Medya Timer" "$notifymsg" -u normal;
-    fi
-    path_to_zenity=$(which zenity)
-    if [ -x "$path_to_zenity" ] ; then
-      zenity --warning --name "Medya Bash Timer" --text="$coremsg"  > /dev/null 2>&1;
-    fi
+	fi
 
-    #
-    # For MacOs
-    # TODO use the coremsg for MacOs
-    #
-  
-    path_to_osascript=$(which osascript)
-    if [ -x "$path_to_osascript" ] ; then
-      osascript -e 'display notification "Ding Ding Ding Your time is up !" with title "Medya Timer"'
-      osascript -e 'tell app "System Events" to display dialog "Your time is up !"'
-    fi
- }
+	#
+	# For MacOs
+	# TODO use the coremsg for MacOs
+	#
+
+	path_to_osascript=$(which osascript)
+	if [ -x "$path_to_osascript" ]; then
+		osascript -e 'display notification "Ding Ding Ding Your time is up !" with title "Medya Timer"'
+		osascript -e 'tell app "System Events" to display dialog "Your time is up !"'
+	fi
+}
