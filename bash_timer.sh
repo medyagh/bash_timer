76b73c8992325165d588466b4a72759efe616412
Medya Gh <medyagh@users.noreply.github.com>
2018-06-15 16:25:10 -0700

    Fixing Mac Issue parsing s and m

--- bash_timer.sh
+++ bash_timer.sh
@@ -1,5 +1,23 @@
 
 timer() {
+	arg1=$@
+	number_part=${arg1//[!0-9]/}
+	letter_part=${arg1//[^a-zA-Z]/}
+	echo $letter_part
+
+	#TODO: add min, hour lower and uppercase
+
+	if [ "$letter_part" == "s" ]; then
+		SECONDS_TO_COUNT_DOWN=$number_part
+	fi
+	if [ "$letter_part" == "m" ]; then
+		SECONDS_TO_COUNT_DOWN=$(($number_part * 60))
+	fi
+
+	if [ "$letter_part" == "h" ]; then
+		SECONDS_TO_COUNT_DOWN=$(($number_part * 60))
+	fi
+
 	coremsg="Your "$@"  is up."
 	notifymsg="
        -------------------
@@ -19,12 +37,11 @@ timer() {
        -------------------
        "
 
-	for i in $(seq $@ $END); do 
-	echo "$i/$@" > /tmp/timer.log
-	sleep 1
+	for i in $(seq $SECONDS_TO_COUNT_DOWN $END); do
+		echo "$i/$@" >/tmp/timer.log
+		sleep 1
 	done
 
-	
 	# Linux
 	path_to_notify_send=$(which notify-send)
 	if [ -x "$path_to_notify_send" ]; then
